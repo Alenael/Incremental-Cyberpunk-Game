35d3ca9b74af642580e6a793d542041fa8e59238
Alenael <Alenael>
2023-03-04 13:22:00 -0500

    Fixes

--- .nano-staged.mjs
+++ .nano-staged.mjs
@@ -11,14 +11,12 @@ export default {
   '{package-lock.json,packages/**/{*.ts,*.vue,tsconfig.json}}': ({filenames}) => {
     // if dependencies was changed run type checking for all packages
     if (filenames.some(f => f.endsWith('package-lock.json'))) {
-      return ['yarn run typecheck --if-present'];
+      return ['yarn run typecheck'];
     }
 
     // else run type checking for staged packages
     const fileNameToPackageName = filename =>
       filename.replace(resolve(process.cwd(), 'packages') + sep, '').split(sep)[0];
-    return [...new Set(filenames.map(fileNameToPackageName))].map(
-      p => `yarn run typecheck:${p} --if-present`,
-    );
+    return [...new Set(filenames.map(fileNameToPackageName))].map(p => `yarn run typecheck:${p}`);
   },
 };
